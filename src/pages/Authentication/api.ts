b8a30ff986a3d4eba46a1b1e54b1005c4f3d2967
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-08-27 22:25:18 -0300

    27/08

--- src/pages/Authentication/api.ts
+++ src/pages/Authentication/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3005/",
+  baseURL: "https://financas-back.onrender.com/",
+  // baseURL: "http://localhost:3005/",
   withCredentials: true,
 });
